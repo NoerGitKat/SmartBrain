e5e4eb13626ebf870ddd0f11c0a5d886c97387e2
Noer Paanakker <noer@healthyworkers.nl>
2018-04-25 22:02:27 +0200

    solved signin

--- src/server.js
+++ src/server.js
@@ -37,34 +37,29 @@ server.post("/signin", (req, response) => {
     .then(data => {
       let foundEmail;
       let matchedPassword;
+      let userId;
       for (let x = 0; x < data.length; x++) {
         if (email === data[x].email) {
           foundEmail = true;
+          userId = x;
         }
-        bcrypt.compare(password, data[x].hash, (err, res) => {
-          // console.log(`res${x}:`, res);
-          console.log("res", res);
-          if (res === true) {
-            matchedPassword = true;
-          }
-          // console.log("matchedPassword isss.....:", matchedPassword);
-        });
       }
-      console.log("foundEmail", foundEmail);
-      console.log("matchedPassword", matchedPassword);
-      if (foundEmail && matchedPassword) {
-        db
-          .select("*")
-          .from("users")
-          .where("email", email)
-          .then(user => {
-            return response.status(200).json(user);
-          });
-      } else {
-        return response
-          .status(400)
-          .json({ message: "Submitted wrong credentials!" });
-      }
+      bcrypt.compare(password, data[userId].hash, (err, res) => {
+        matchedPassword = res;
+        if (foundEmail && matchedPassword) {
+          db
+            .select("*")
+            .from("users")
+            .where("email", email)
+            .then(user => {
+              return response.status(200).json(user);
+            });
+        } else {
+          return response
+            .status(400)
+            .json({ message: "Submitted wrong credentials!" });
+        }
+      });
     })
     .catch(err => console.log("err", err));
 });
